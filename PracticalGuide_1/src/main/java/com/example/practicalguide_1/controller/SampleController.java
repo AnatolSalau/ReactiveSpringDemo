824711e235e4ba98d3882843a5fd4a20d8a98bfb
AnatolSalau <salovanatoly5@mail.ru>
2024-09-23 17:13:38 +0300

    Add Tutorial

---
+++ PracticalGuide_1/src/main/java/com/example/practicalguide_1/controller/SampleController.java
@@ -0,0 +1,45 @@
+package com.example.practicalguide_1.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@RestController
+public class SampleController {
+    @GetMapping("/sample1")
+    Mono<String> sample1() {
+        return Mono.just("Hello world");
+    }
+
+    @GetMapping(value = "/sample3", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    Flux<Integer> sample3() {
+        return Flux
+                .just(1,2,3,4,5,6,7,8,9,10)
+                .delayElements(Duration.ofSeconds(1L))
+                .log();
+    }
+
+    @GetMapping(value = "/sample2", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<ServerSentEvent<String>> sample2() {
+        Flux<Integer> integerFlux = Flux
+                .just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .delayElements(Duration.ofSeconds(1L));
+
+        Flux<ServerSentEvent<String>> message = integerFlux.map(
+                i -> {
+                    return ServerSentEvent.<String>builder()
+                            .id(String.valueOf(i))
+                            .event("message")
+                            .data("Event #" + i + " at " + LocalDateTime.now())
+                            .build();
+                }
+        );
+        return message;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 ## HELP.md
 ### Reactive Spring Demo from medium
-1.[Start](https://medium.com/javarevisited/spring-webflux-tutorial-how-to-build-a-reactive-web-app-1132e4255406)
+
+1. [Start](https://medium.com/javarevisited/spring-webflux-tutorial-how-to-build-a-reactive-web-app-1132e4255406)
+
+2. [Introduction](https://gatheca-george.medium.com/spring-webflux-reactive-micro-service-introduction-3ad0659169df)
+
+3. [Practical guide Intro](https://gatheca-george.medium.com/spring-webflux-reactive-micro-service-practical-guide-intro-eeadf733a226)
+    - [github](https://github.com/gathecageorge/spring-webflux-rest-api-intro/tree/2-practical-guide-intro/)
 
 
 
