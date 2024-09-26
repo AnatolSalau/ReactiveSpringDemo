8c44d703585401f13788f2e84c765b9cbbdc14e3
AnatolSalau <salovanatoly5@mail.ru>
2024-09-26 17:04:26 +0300

    Post User

---
+++ PracticalGuide_1/src/main/java/com/example/practicalguide_1/PracticalGuide1Application.java
@@ -0,0 +1,13 @@
+package com.example.practicalguide_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalGuide1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalGuide1Application.class, args);
+	}
+
+}
